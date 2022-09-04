ac6961ae852c4fe05f75b797c68d45e767197b22
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-04 14:46:43 -0300

    add route para insert da api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('books', [BooksController::class, 'index']);
+Route::post('books', [BooksController::class, 'store']);
 Route::get('books/{id}', [BooksController::class, 'show']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
