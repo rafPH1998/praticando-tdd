35cb1208e26630675d87e62d135b12135738e182
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-07 22:11:54 -0300

    Routes

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\Api\BooksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('books', [BooksController::class, 'index']);
-Route::post('books', [BooksController::class, 'store']);
-Route::get('books/{id}', [BooksController::class, 'show']);
+Route::apiResource('books', BooksController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
