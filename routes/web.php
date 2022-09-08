3e5381bd6709b37490597f42a4cc9181c11797ec
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-07 22:12:59 -0300

    Routes para RegisterController

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::post('register', RegisterController::class)->name('register');
+Route::get('home', RegisterController::class)->name('home');
 
 Route::get('/', function () {
     return view('welcome');
