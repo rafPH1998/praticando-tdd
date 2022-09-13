39b18fd9bb7982dafa838af6e0a79901f4fa38e4
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-13 15:21:40 -0300

    news validations for tests

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterController extends Controller
 {
     public function __invoke(Request $request)
     {
         $request->validate([
-            'name' => 'required|max:255'
+            'name' => ['required', 'max:20'],
+            'email' => ['required', 'email', 'unique:users', 'confirmed'],
+            'password' => ['required', 'string', Password::min(4)->mixedCase()],
         ]);
         
         $data = $request->all();
