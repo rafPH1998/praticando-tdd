08042f9810e6158b001fbd41321636304ff9161b
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-14 19:29:35 -0300

    routes for tests

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\RegisterController;
-use App\Models\User;
+use App\Http\Controllers\Todo\CreateController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', RegisterController::class)->name('register');
 Route::get('home', RegisterController::class)->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function() {
+    Route::get('todo', [CreateController::class, 'index'])->name('todo.index');
+    Route::post('todo', [CreateController::class, 'store'])->name('todo.store');
+    Route::put('todo/{todo}', [CreateController::class, 'update'])->name('todo.update');
+    Route::delete('todo/{todo}', [CreateController::class, 'destroy'])->name('todo.destroy');
 });
