360375e0ba2f0b6ba2ea81e382dc30592947d270
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-16 16:09:55 -0300

    regra de validação para cnpj

---
+++ app/Rules/ValidCNPJ.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Rules;
+
+use App\Services\BrasilAPI\Enum\CNPJSituacaoCadastral;
+use App\Services\BrasilAPI\BrasilAPI;
+use Illuminate\Contracts\Validation\Rule;
+use App\Services\BrasilAPI\Exceptions\CPNJNotFound;
+
+class ValidCNPJ implements Rule
+{
+
+    public function passes($attribute, $value)
+    {
+        
+        try {
+            $cpnj = (new BrasilAPI)->cpnj($value);
+
+            return $cpnj->descricaoSituacaoCadastral == CNPJSituacaoCadastral::ATIVO;
+
+        } catch (CPNJNotFound $error) {
+            return false;
+        }
+    }
+
+    public function message()
+    {
+        return 'The validation error message.';
+    }
+}
