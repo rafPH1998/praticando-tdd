ac2c771b317abbff9a64f42932bac418b4efb2c6
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-16 21:49:10 -0300

    cnpj na migration

--- database/migrations/2022_09_03_185647_create_books_table.php
+++ database/migrations/2022_09_03_185647_create_books_table.php
@@ -15,8 +15,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('isbn');
+            $table->string('cnpj');
             $table->timestamps();
         });
     }
